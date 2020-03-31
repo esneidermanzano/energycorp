64e03cd8ff2720cf860826d397b8b8fac88cc650
SH-Arias <hernandez.santiago@correounivalle.edu.co>
2020-03-31 11:55:14 -0500

    third commit
    Inside src
    - App
    - index
    - logo.PNG
    - logo.HTML
    
    Created asstes folder
    - created css folder

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
+
+import DashLayout from "layouts/DashLayout.jsx";
+import Start from "views/Start.jsx";
+import Login from "components/login/Login.jsx";
+import GetBill from "components/GetBill.jsx";
+
+import routesAdmin from "routes/adminRoutes.js";
+import routesOperator from "routes/operatorRoutes.js";
+import routesManager from "routes/managerRoutes.js";
+
+import { ProtectedRoute, ProtectedLoginRoute } from "components/auth/ProtectedRoute.js";
+
+const hist = createBrowserHistory();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router history={hist}>
+            <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==" crossOrigin="" />
+            <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js" integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew==" crossOrigin=""></script>
+
+            <Switch>
+                <Route exact path="/" component={Start} />
+                <ProtectedLoginRoute exact path="/login" component={Login} />
+                <Route exact path="/getBill" component={GetBill} />
+
+                <ProtectedRoute path="/admin" userType="admin" render={props => <DashLayout {...props} routes={routesAdmin} loggedInBtn={true} />} />
+                <ProtectedRoute path="/operator" userType="operator" render={props => <DashLayout {...props} routes={routesOperator} loggedInBtn={true} />} />
+                <ProtectedRoute path="/manager" userType="manager" render={props => <DashLayout {...props} routes={routesManager} loggedInBtn={true} />} />
+
+                <Redirect exact to="/" />
+            </Switch>
+        </Router>
+    )
 }
 
 export default App;
--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+/*!
+
+=========================================================
+* Paper Dashboard React - v1.1.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/paper-dashboard-react
+* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+
+* Licensed under MIT (https://github.com/creativetimofficial/paper-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+import ReactDOM from "react-dom";
+// import { createBrowserHistory } from "history";
+// import { Router, Route, Switch, Redirect } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "assets/scss/paper-dashboard.scss?v=1.1.0";
+import "assets/demo/demo.css";
+import "assets/css/index.css";
+import "perfect-scrollbar/css/perfect-scrollbar.css";
+import "assets/css/animate.css";
+import "assets/css/login.css";
+
+import App from 'App.js';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
