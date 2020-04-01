0b7d1921b2478324208f57df4bcdaf4996ae9ce8
esneidermanzano <esneidermanzano@gmail.com>
2020-04-01 08:57:35 -0500

    build: add cors headers

--- Backend/src/rest/settings.py
+++ Backend/src/rest/settings.py
@@ -44,10 +44,11 @@ INSTALLED_APPS = [
     'rest_framework',
     'users',
     'rest_framework.authtoken'
-
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
