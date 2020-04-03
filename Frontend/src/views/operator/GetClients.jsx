7c6fbfe2440fa90abc095106936be99a72e37d9a
carban <murillo.carlos@correounivalle.edu.co>
2020-04-03 01:23:56 -0500

    feat: getClients improve
    getCLients

--- Frontend/src/views/operator/GetClients.jsx
+++ Frontend/src/views/operator/GetClients.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import CreateClientForm from "views/operator/CreateClientForm.jsx";
+import axios from 'axios';
+
 
 import {
     Card,
@@ -13,108 +15,7 @@ class GetClients extends React.Component {
     constructor(props) {
         super(props);
         this.state = { //PHONE IS NUMBER
-            persons: [
-                {
-                    ID: 121,
-                    name: 'charles',
-                    lastname: 'pirlo',
-                    email: 'murillo.carlos@correounivalle.edu.co',
-                    adress: 'sdsds',
-                    phone: 'dfdfdfd',
-                    sex: 'Male',
-                    date: '2020-02-26'
-                },
-                {
-                    ID: 232,
-                    name: 'charles',
-                    lastname: 'pirlo',
-                    email: 'messi.carlos@correounivalle.edu.co',
-                    adress: 'sdsds',
-                    phone: 'dfdfdfd',
-                    sex: 'Male',
-                    date: '2020-02-26'
-                },
-                {
-                    ID: 233,
-                    name: 'werwe',
-                    lastname: 'pirlo',
-                    email: 'james.carlos@correounivalle.edu.co',
-                    adress: 'sdsds',
-                    phone: 'dfdfdfd',
-                    sex: 'Female',
-                    date: '2020-02-26'
-                },
-                {
-                    ID: 344,
-                    name: 'charles',
-                    lastname: 'pirlo',
-                    email: 'ronaldo@cristiano.com',
-                    adress: 'sdsds',
-                    phone: 'dfdfdfd',
-                    sex: 'Male',
-                    date: '2020-02-26'
-                },
-                {
-                    ID: 345,
-                    name: 'chaertertreterles',
-                    lastname: 'pirlo',
-                    email: 'zlatan@correounivalle.edu.co',
-                    adress: 'sdsds',
-                    phone: 'dfdfdfd',
-                    sex: 'Female',
-                    date: '2020-02-26'
-                },
-                {
-                    ID: 346,
-                    name: 'rtertete',
-                    lastname: 'pirlo',
-                    email: 'rodallega@correounivalle.edu.co',
-                    adress: 'sdsds',
-                    phone: 'dfdfdfd',
-                    sex: 'Female',
-                    date: '2020-02-26'
-                },
-                {
-                    ID: 347,
-                    name: 'charles',
-                    lastname: 'pirlo',
-                    email: 'maradona@gmail.com',
-                    adress: 'sdsds',
-                    phone: 'dfdfdfd',
-                    sex: 'Male',
-                    date: '2020-02-26'
-                },
-                {
-                    ID: 457,
-                    name: 'charles',
-                    lastname: 'pirlo',
-                    email: 'modric@gmail.com',
-                    adress: 'sdsds',
-                    phone: 'dfdfdfd',
-                    sex: 'Male',
-                    date: '2020-02-26'
-                },
-                {
-                    ID: 458,
-                    name: 'charles',
-                    lastname: 'pirlo',
-                    email: 'suarez@gmail.com',
-                    adress: 'sdsds',
-                    phone: 'dfdfdfd',
-                    sex: 'Male',
-                    date: '2020-02-26'
-                },
-                {
-                    ID: 569,
-                    name: 'charles',
-                    lastname: 'pirlo',
-                    email: 'parasite@gmail.com',
-                    adress: 'sdsds',
-                    phone: 'dfdfdfd',
-                    sex: 'Male',
-                    date: '2020-02-26'
-                },
-            ],
+            persons: [],
             search: '',
             modal: false,
             selected: 'All',
@@ -141,9 +42,24 @@ class GetClients extends React.Component {
         });
     }
 
-    editUser = () => {
-        alert('Axios');
+    editUser = state => {
+        // alert('Axios');
+        // axios.post("https://energycorp.herokuapp.com/api/user/client/" + state.id + "/update/", state)
+        //     .then(res => {
+        //         alert("AXION REALIZADA CON EXITO");
+        //     })
+        //     .catch(err => {
+        //         console.log(err);
+        //     })
         this.closeToggle();
+    }
+
+    async componentDidMount() {
+        const res = await fetch('https://energycorp.herokuapp.com/api/user/client/');
+
+        const data = await res.json();
+
+        this.setState({ persons: data });
     }
 
     render() {
@@ -151,16 +67,16 @@ class GetClients extends React.Component {
         var filteredPeople = [];
 
         filteredPeople = this.state.persons.filter(p => (
-            p.email.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1
+            p.user.email.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1
         ));
 
         const people = filteredPeople.map((p, k) => (
             <tr key={k}>
                 <th scope="row">#{k + 1}</th>
-                <th>{p.name}</th>
-                <th>{p.lastname}</th>
-                <th>{p.ID}</th>
-                <th>{p.email}</th>
+                <th>{p.user.id_user}</th>
+                <th>{p.user.name}</th>
+                <th>{p.user.phone}</th>
+                <th>{p.user.email}</th>
                 <th>
                     <Button color="warning" onClick={() => this.openToggle(p)}>
                         <i className="nc-icon nc-zoom-split" />
@@ -187,11 +103,11 @@ class GetClients extends React.Component {
                                         <thead>
                                             <tr>
                                                 <th>#</th>
-                                                <th>First Name</th>
-                                                <th>Last Name</th>
                                                 <th>ID</th>
+                                                <th>Name</th>
+                                                <th>Phone</th>
                                                 <th>Email</th>
-                                                <th>Type</th>
+                                                <th>Review</th>
                                             </tr>
                                         </thead>
                                         <tbody>
@@ -207,7 +123,7 @@ class GetClients extends React.Component {
                         <Modal md="7" isOpen={this.state.modal} toggle={this.closeToggle} className="danger">
                             <ModalHeader toggle={this.closeToggle}>Edit User</ModalHeader>
                             <ModalBody>
-                                <CreateClientForm submitAction={this.editUser} user={this.state.user} />
+                                <CreateClientForm submitAction={this.editUser} user={this.state.user} editMode={true} />
                             </ModalBody>
                         </Modal>
                     </div>
