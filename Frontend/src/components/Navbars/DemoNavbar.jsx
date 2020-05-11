0777c2cf572992c99fd3d977cacf5d065a1681a7
carban <murillo.carlos@correounivalle.edu.co>
2020-05-11 15:50:27 -0500

    feat: navbar select language

--- Frontend/src/components/Navbars/DemoNavbar.jsx
+++ Frontend/src/components/Navbars/DemoNavbar.jsx
@@ -33,10 +33,19 @@ import {
   InputGroup,
   InputGroupText,
   InputGroupAddon,
-  Input, Button
+  Input, Button, Row, Col
 } from "reactstrap";
 
 import auth from "components/auth/auth.js";
+
+
+import { connect } from "react-redux";
+
+
+import en_icon from "./england_icon.png";
+import es_icon from "./spain_icon.png";
+import po_icon from "./portuguese_icon.png";
+
 
 
 // import routes from "routes/routes.js";
@@ -53,6 +62,7 @@ class Header extends React.Component {
     this.dropdownToggle = this.dropdownToggle.bind(this);
     this.sidebarToggle = React.createRef();
   }
+
   toggle() {
     if (this.state.isOpen) {
       this.setState({
@@ -117,99 +127,137 @@ class Header extends React.Component {
   render() {
 
     const loggedInBtn = this.props.loggedInBtn ?
-      <Button onClick={() => { 
-        auth.logout(() => { this.props.history.push("/login") }) 
+      <Button onClick={() => {
+        auth.logout(() => { this.props.history.push("/login") })
       }}>Logout</Button>
       :
       <Link className="btn" to="/login">Login</Link>
 
-  return(
+    return (
       // add or remove classes depending if we are on full-screen-maps page or not
       <Navbar
-        color = {
-      this.props.location.pathname.indexOf("full-screen-maps") !== -1
-        ? "dark"
-        : this.state.color
-    }
-        expand = "lg"
-        className = {
-      this.props.location.pathname.indexOf("full-screen-maps") !== -1
-        ? "navbar-absolute fixed-top"
-        : "navbar-absolute fixed-top " +
-        (this.state.color === "transparent" ? "navbar-transparent " : "")
-    }
+        color={
+          this.props.location.pathname.indexOf("full-screen-maps") !== -1
+            ? "dark"
+            : this.state.color
+        }
+        expand="lg"
+        className={
+          this.props.location.pathname.indexOf("full-screen-maps") !== -1
+            ? "navbar-absolute fixed-top"
+            : "navbar-absolute fixed-top " +
+            (this.state.color === "transparent" ? "navbar-transparent " : "")
+        }
       >
-      <Container fluid>
-        <div className="navbar-wrapper">
-          <div className="navbar-toggle">
-            <button
-              type="button"
-              ref={this.sidebarToggle}
-              className="navbar-toggler"
-              onClick={() => this.openSidebar()}
-            >
-              <span className="navbar-toggler-bar bar1" />
-              <span className="navbar-toggler-bar bar2" />
-              <span className="navbar-toggler-bar bar3" />
-            </button>
+        <Container fluid>
+          <div className="navbar-wrapper">
+            <div className="navbar-toggle">
+              <button
+                type="button"
+                ref={this.sidebarToggle}
+                className="navbar-toggler"
+                onClick={() => this.openSidebar()}
+              >
+                <span className="navbar-toggler-bar bar1" />
+                <span className="navbar-toggler-bar bar2" />
+                <span className="navbar-toggler-bar bar3" />
+              </button>
+            </div>
+            <NavbarBrand href="/">{this.getBrand()}</NavbarBrand>
           </div>
-          <NavbarBrand href="/">{this.getBrand()}</NavbarBrand>
-        </div>
-        <NavbarToggler onClick={this.toggle}>
-          <span className="navbar-toggler-bar navbar-kebab" />
-          <span className="navbar-toggler-bar navbar-kebab" />
-          <span className="navbar-toggler-bar navbar-kebab" />
-        </NavbarToggler>
-        <Collapse
-          isOpen={this.state.isOpen}
-          navbar
-          className="justify-content-end"
-        >
-          <form>
-            <InputGroup className="no-border">
-              <Input placeholder="Search..." />
-              <InputGroupAddon addonType="append">
-                <InputGroupText>
-                  <i className="nc-icon nc-zoom-split" />
-                </InputGroupText>
-              </InputGroupAddon>
-            </InputGroup>
-          </form>
-          <Nav navbar>
-            <NavItem>
-              <Link to="#pablo" className="nav-link btn-magnify">
-                <i className="nc-icon nc-layout-11" />
-                <p>
-                  <span className="d-lg-none d-md-block">Stats</span>
-                </p>
-              </Link>
-            </NavItem>
-            <Dropdown
-              nav
-              isOpen={this.state.dropdownOpen}
-              toggle={e => this.dropdownToggle(e)}
-            >
-              <DropdownToggle caret nav>
-                <i className="nc-icon nc-bell-55" />
-                <p>
-                  <span className="d-lg-none d-md-block">Some Actions</span>
-                </p>
-              </DropdownToggle>
-              <DropdownMenu right>
-                <DropdownItem tag="a">Action</DropdownItem>
-                <DropdownItem tag="a">Another Action</DropdownItem>
-                <DropdownItem tag="a">Something else here</DropdownItem>
-              </DropdownMenu>
-            </Dropdown>
-            <NavItem>
-            {loggedInBtn}
-            </NavItem>
-          </Nav>
-        </Collapse>
-      </Container>
+          <NavbarToggler onClick={this.toggle}>
+            <span className="navbar-toggler-bar navbar-kebab" />
+            <span className="navbar-toggler-bar navbar-kebab" />
+            <span className="navbar-toggler-bar navbar-kebab" />
+          </NavbarToggler>
+          <Collapse
+            isOpen={this.state.isOpen}
+            navbar
+            className="justify-content-end"
+          >
+            {/* <form>
+              <InputGroup className="no-border">
+                <Input placeholder="Search..." />
+                <InputGroupAddon addonType="append">
+                  <InputGroupText>
+                    <i className="nc-icon nc-zoom-split" />
+                  </InputGroupText>
+                </InputGroupAddon>
+              </InputGroup>
+            </form> */}
+            <Nav navbar>
+              {/* <NavItem>
+                <Link to="#pablo" className="nav-link btn-magnify">
+                  <i className="nc-icon nc-layout-11" />
+                  <p>
+                    <span className="d-lg-none d-md-block">Stats</span>
+                  </p>
+                </Link>
+              </NavItem> */}
+              <Dropdown
+                nav
+                isOpen={this.state.dropdownOpen}
+                toggle={e => this.dropdownToggle(e)}
+              >
+                <DropdownToggle caret nav>
+                  <i className="nc-icon nc-caps-small" />
+                  <p>
+                    <span className="d-lg-none d-md-block">Some Actions</span>
+                  </p>
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem tag="a"  onClick={() => this.props.changeLng('en')}>
+                    <Row>
+                      <Col>
+                        <img src={en_icon} alt="" width="30px" height="30px" />
+                      </Col>
+                      <Col>
+                        English
+                      </Col>
+                    </Row>
+                  </DropdownItem>
+                  <DropdownItem tag="a" onClick={() => this.props.changeLng('es')}>
+                    <Row>
+                      <Col>
+                        <img src={es_icon} alt="" width="30px" height="30px" />
+                      </Col>
+                      <Col>
+                        Spanish
+                      </Col>
+                    </Row>
+                  </DropdownItem>
+                  <DropdownItem tag="a" onClick={() => this.props.changeLng('po')}>
+                    <Row>
+                      <Col>
+                        <img src={po_icon} alt="" width="30px" height="30px" />
+                      </Col>
+                      <Col sm="7">
+                        Portuguese
+                      </Col>
+                    </Row>
+                  </DropdownItem>
+                </DropdownMenu>
+              </Dropdown>
+              {/* <NavItem> */}
+                {loggedInBtn}
+              {/* </NavItem> */}
+            </Nav>
+          </Collapse>
+        </Container>
       </Navbar>
     );
   }
 }
 
-export default Header;
+const mapStateToProps = state => ({})
+
+const mapDispatchToProps = dispatch => ({
+  changeLng(lan) {
+    dispatch({
+      type: "CHANGE_LAN",
+      language: lan
+    })
+  }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
