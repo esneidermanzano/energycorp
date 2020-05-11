135f816c204a0eb51d7c5a49246604ddb5d3b936
carban <murillo.carlos@correounivalle.edu.co>
2020-05-11 15:52:03 -0500

    feat: changes on start with translators, redux and bot

--- Frontend/src/views/Start.jsx
+++ Frontend/src/views/Start.jsx
@@ -1,12 +1,38 @@
 import React from "react";
 import DemoNavbar from "components/Navbars/DemoNavbar.jsx";
+import ChatBot from "components/chatBot/ChatBot.jsx";
 
 import { Link } from "react-router-dom";
+
+import counterpart from "counterpart";
+import * as Tr from "react-translate-component";
+import spanish from "../langs/spanish.js";
+import english from "../langs/english.js";
+
+import bot_gif from "./bot_gif.gif";
+
+import { connect } from "react-redux";
+
+// counterpart.registerTranslations('en', english);
+// counterpart.registerTranslations('es', spanish);
+
+ 
+// counterpart.setLocale('en');
 
 class Start extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            visibleChat: false
+        }
+    }
+
+    openChat = () => {
+        this.setState({ visibleChat: true });
+    }
+
+    closeChat = () => {
+        this.setState({ visibleChat: false });
     }
 
     render() {
@@ -15,15 +41,34 @@ class Start extends React.Component {
                 <DemoNavbar {...this.props} />
                 <div className="contentStart">
                     <center>
-                        <h1>El Mejor Servicio a tu Alcance</h1>
+                        <Tr content="home.title" component="h1" />
                         <img width="744" height="490" src="https://www.mercapava.com.co/wp-content/uploads/2016/10/Tip-5.jpg" alt="description"></img>
                         <br></br>
-                        <Link className="btn btn-danger" to="/getBill">Download your Bill!</Link>
+                        <br></br>
                     </center>
                 </div>
+                {
+                    this.state.visibleChat
+                        ? <ChatBot close={this.closeChat} />
+                        : <img onClick={this.openChat} className="floatingBot animated wobble" src={bot_gif} width="95px" height="120px" alt="description"></img>
+                }
+
+                <center>
+                    <br />
+                    <Link className="btn btn-danger" to="/getBill">
+                        <Tr content="home.download" component="div" />
+                    </Link>
+                </center>
             </div>
         )
     }
 }
 
-export default Start;
+const mapStateToProps = state => {
+    counterpart.setLocale(state.language);
+    return { lng: state.language }
+}
+
+const mapDispatchToProps = dispatch => ({});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Start);
