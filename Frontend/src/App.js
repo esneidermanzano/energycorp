6cf546c414fd4c87731dc9208ef9408de89a7575
carban <murillo.carlos@correounivalle.edu.co>
2020-05-11 15:59:28 -0500

    fix: fixing app.js for store issue

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -13,26 +13,31 @@ import routesManager from "routes/managerRoutes.js";
 
 import { ProtectedRoute, ProtectedLoginRoute } from "components/auth/ProtectedRoute.js";
 
+import { Provider } from "react-redux";
+import store from "./store";
+
 const hist = createBrowserHistory();
 
 function App() {
     return (
-        <Router history={hist}>
-            <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==" crossOrigin="" />
-            <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js" integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew==" crossOrigin=""></script>
+        <Provider store={store}>
+            <Router history={hist}>
+                <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==" crossOrigin="" />
+                <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js" integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew==" crossOrigin=""></script>
 
-            <Switch>
-                <Route exact path="/" component={Start} />
-                <ProtectedLoginRoute exact path="/login" component={Login} />
-                <Route exact path="/getBill" component={GetBill} />
+                <Switch>
+                    <Route exact path="/" component={Start} />
+                    <ProtectedLoginRoute exact path="/login" component={Login} />
+                    <Route exact path="/getBill" component={GetBill} />
 
-                <ProtectedRoute path="/admin" userType="admin" render={props => <DashLayout {...props} routes={routesAdmin} loggedInBtn={true} />} />
-                <ProtectedRoute path="/operator" userType="operator" render={props => <DashLayout {...props} routes={routesOperator} loggedInBtn={true} />} />
-                <ProtectedRoute path="/manager" userType="manager" render={props => <DashLayout {...props} routes={routesManager} loggedInBtn={true} />} />
+                    <ProtectedRoute path="/admin" userType="admin" render={props => <DashLayout {...props} routes={routesAdmin} loggedInBtn={true} />} />
+                    <ProtectedRoute path="/operator" userType="operator" render={props => <DashLayout {...props} routes={routesOperator} loggedInBtn={true} />} />
+                    <ProtectedRoute path="/manager" userType="manager" render={props => <DashLayout {...props} routes={routesManager} loggedInBtn={true} />} />
 
-                <Redirect exact to="/" />
-            </Switch>
-        </Router>
+                    <Redirect exact to="/" />
+                </Switch>
+            </Router>
+        </Provider>
     )
 }
 
