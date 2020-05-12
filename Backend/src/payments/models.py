a80570e239f5539568c7f8777ec6640718f1c639
Luis <luis.alberto.gomez@correounivalle.edu.co>
2020-05-12 09:52:46 -0500

    feat: adding module payment

---
+++ Backend/src/payments/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from factures.models import InvoiceServices
+from users.models import Worker
+
+# Create your models here.
+""" The models in this File are the objetcs than respresent the pago,
+    pago directo, pago banco."""
+
+
+class Payment(models.Model):
+    """Modelo para representar el objero Pago"""
+    codePayment = models.AutoField(
+        auto_created=True,
+        primary_key=True,
+        serialize=False,
+        verbose_name='ID'
+    )
+    valuePayment = models.PositiveIntegerField()
+    datePayment = models.DateTimeField(auto_now_add=True)
+    facturaPayment = models.OneToOneField("app.Model",
+                                          on_delete=models.CASCADE)
+
+
+class DirectPayment(models.Model):
+    """Modelo para representar el objero Pago"""
+    codeDirectPayment = models.AutoField(
+        auto_created=True,
+        primary_key=True,
+        serialize=False,
+        verbose_name='ID'
+    )
+    payment = models.OneToOneField(
+        Payment, on_delete=models.CASCADE)
+    workerPayment = models.ForeignKey(Worker, on_delete=models.CASCADE)
+    
+
+
+class BanckPayment(models.Model):
+    """Modelo para representar el objero Pago"""
+    codePayment = models.AutoField(
+        auto_created=True,
+        primary_key=True,
+        serialize=False,
+        verbose_name='ID'
+    )
+    valuePayment = models.PositiveIntegerField()
+    datePayment = models.DateTimeField(auto_now_add=True)
+    facturaPayment = models.OneToOneField("app.Model",
+                                          on_delete=models.CASCADE)
