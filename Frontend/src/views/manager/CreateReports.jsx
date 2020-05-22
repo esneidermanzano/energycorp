f4f7afda053aadafde8dedaa06d5c5cdac3d593e
SH-Arias <hernandez.santiago@correounivalle.edu.co>
2020-05-22 00:56:10 -0500

    Feat Code Cleaning

--- Frontend/src/views/manager/CreateReports.jsx
+++ Frontend/src/views/manager/CreateReports.jsx
@@ -31,27 +31,7 @@ class CreateReports extends React.Component {
             'Suspended Users'
           ]
         },
-        barChartDataHighest: {
-          labels: ['#1', '#2', '#3', '#4', '#5'],
-          datasets: [{
-            label: 'Consumo',
-            data: [
-              2000,
-              1700,
-              1500,
-              1200,
-              1000
-            ],
-            backgroundColor: [
-              'rgba(255, 99, 132, 0.6)',
-              'rgba(54, 162, 235, 0.6)',
-              'rgba(255,206, 86, 0.6)',
-              'rgba(75, 192, 192, 0.6)',
-              'rgba(153, 102, 255, 0.6)',
-            ]
-          }
-          ]
-        },
+        barChartDataHighest: {},
         barChartDataLowest: {
           labels: ['#1', '#2', '#3', '#4', '#5'],
           datasets: [{
