15d407afb3203ccb98a75ed4ada6acf7327ba08a
SH-Arias <hernandez.santiago@correounivalle.edu.co>
2020-05-22 01:27:29 -0500

    Feat: Color changes

--- Frontend/src/views/manager/CreateReports.jsx
+++ Frontend/src/views/manager/CreateReports.jsx
@@ -86,11 +86,11 @@ class CreateReports extends React.Component {
           dataBar.topfiveminus[0].value
         ],
         backgroundColor: [
+          'rgba(255,206, 86, 0.6)',
           'rgba(75, 192, 192, 0.6)',
           'rgba(153, 102, 255, 0.6)',
           'rgba(255, 99, 132, 0.6)',
-          'rgba(54, 162, 235, 0.6)',
-          'rgba(255,206, 86, 0.6)',
+          'rgba(54, 162, 235, 0.6)'
         ]
       }
       ]
